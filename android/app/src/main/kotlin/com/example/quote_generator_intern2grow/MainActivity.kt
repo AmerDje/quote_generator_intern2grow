a09f633cf2986f6f2710e5709363f1d8013049e9
AmerDje <amer.dje10n20@gmail.com>
2023-10-16 14:52:28 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/quote_generator_intern2grow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quote_generator_intern2grow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QuoteGeneratorIntern2Grow());
+}
+
+class QuoteGeneratorIntern2Grow extends StatelessWidget {
+  const QuoteGeneratorIntern2Grow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
